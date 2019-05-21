df3490d6d382b19027b3ceb839857639f6023b1b
Vince Via <vince@viaoa.com>
2019-05-20 21:51:31 -0400

    scheduler delegate

---
+++ src/main/java/com/viaoa/object/OAObjectSchedulerDelegate.java
@@ -0,0 +1,59 @@
+package com.viaoa.object;
+
+import java.lang.reflect.Method;
+
+import com.viaoa.scheduler.OAScheduler;
+import com.viaoa.util.OADate;
+import com.viaoa.util.OAString;
+
+/**
+ * Used to call Scheduler methods on OAObject link properties.
+ * @author vvia
+ */
+public class OAObjectSchedulerDelegate {
+
+    public static OAScheduler getScheduler(OAObject objThis, String property, OADate date) {
+        return getScheduler(objThis, property, null, date);
+    }
+    
+    public static OAScheduler getScheduler(OAObject objThis, String property, OAObject objSearch, OADate date) {
+        if (objThis == null || OAString.isEmpty(property)) return null;
+
+        OAObjectInfo oi = OAObjectInfoDelegate.getObjectInfo(objThis);
+        if (oi == null) return null;
+        OALinkInfo li = oi.getLinkInfo(property);
+        if (li == null) return null;
+        
+        Method method = li.getSchedulerMethod();
+        if (method == null) return null;
+        
+        OAScheduler scheduler = new OAScheduler(objSearch, date, date);
+        
+        try {
+            method.invoke(objThis, scheduler);
+        }
+        catch (Exception e) {
+            throw new RuntimeException("exception while invoking scheduler callback method="+method+", for object="+objThis, e);
+        }
+        return scheduler;
+    }
+    
+    public static void invokeCallback(OAScheduler scheduler, OAObject objThis, String property) {
+        if (scheduler == null || objThis == null || OAString.isEmpty(property)) return;
+
+        OAObjectInfo oi = OAObjectInfoDelegate.getObjectInfo(objThis);
+        if (oi == null) return;
+        OALinkInfo li = oi.getLinkInfo(property);
+        if (li == null) return;
+        
+        Method method = li.getSchedulerMethod();
+        if (method == null) return;
+        
+        try {
+            method.invoke(objThis, scheduler);
+        }
+        catch (Exception e) {
+            throw new RuntimeException("exception while invoking scheduler callback method="+method+", for object="+objThis, e);
+        }
+    }
+}
