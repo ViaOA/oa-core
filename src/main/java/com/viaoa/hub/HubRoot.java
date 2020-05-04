4782e9c218d1fd17a323f17a0c9157fb23c2f777
Vince Via <vvia@oreillyauto.com>
2020-05-04 18:49:47 -0400

    fix concurrent issue

--- src/main/java/com/viaoa/hub/HubRoot.java
+++ src/main/java/com/viaoa/hub/HubRoot.java
@@ -10,111 +10,144 @@
 */
 package com.viaoa.hub;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import com.viaoa.object.OALinkInfo;
 import com.viaoa.object.OAObject;
 import com.viaoa.object.OAObjectInfo;
 import com.viaoa.object.OAObjectInfoDelegate;
+import com.viaoa.util.OALogger;
 
 /**
- * Used for recursive Hubs, so that the hub is always using the rootHub.
- * The default behavior when using a recursive Hub is that the hub will be shared to 
- * whatever hub the AO is set to.  This will allow a hubRoot to always be the top level hub.
-
- * example:
-    // get the original root
-    this.hub = hubModel.getDetailHub(Model.P_Containers).createSharedHub();
-    // create empty hub that will then always contain the root hub objects
-    this.hubRoot = new Hub&lt;Container&gt;(Container.class);
-    new HubRoot(hub, hubRoot);  // hubRoot will always have top level hub, initially using hub
-    ...
-    OATreeNode node = new OATreeNode(App.P_Label, hubRoot, hub) ... 
- * 
-   
- * @author vvia
- * 20120302
+ * Used for recursive Hubs, so that the hub is always using the rootHub. The default behavior when using a recursive Hub is that the hub
+ * will be shared to whatever hub the AO is set to. This will allow a hubRoot to always be the top level hub. example: // get the original
+ * root this.hub = hubModel.getDetailHub(Model.P_Containers).createSharedHub(); // create empty hub that will then always contain the root
+ * hub objects this.hubRoot = new Hub&lt;Container&gt;(Container.class); new HubRoot(hub, hubRoot); // hubRoot will always have top level
+ * hub, initially using hub ... OATreeNode node = new OATreeNode(App.P_Label, hubRoot, hub) ...
+ *
+ * @author vvia 20120302
  */
 public class HubRoot {
-    private Hub hubRoot;
-    private Hub hubMaster;
-    private HubCopy hubCopy;
-    private String propertyFromMaster;
-    private HubListener hubListener;
-    
-    /**
-     * This is used for recursive hubs, so that a Hub will stay at the root.
-     * By default, a shared hub that is recursive could change to be shared with a child hub.
-     * This class is used to make sure that the hub does not change to share a child hub.
-     * @param hubRoot Hub to use as the root, it will auto populated to always be the root hub.
-     */
-    public HubRoot(Hub hub, Hub hubRoot) {
-        if (hub == null) return;
-        if (hubRoot == null) return;
+	private static final Logger LOG = OALogger.getLogger(HubRoot.class);
 
-        this.hubRoot = hubRoot;
-        
-        Class clazz = hub.getObjectClass();
-        OAObjectInfo oi = OAObjectInfoDelegate.getOAObjectInfo(clazz);
-        OALinkInfo li = oi.getRecursiveLinkInfo(OALinkInfo.MANY);
-        if (li == null) {
-            hubRoot.setSharedHub(hub, true);
-            return;
-        }
+	private final Hub hubRoot;
+	private Hub hubMaster;
+	private volatile HubCopy hubCopy;
+	private String propertyFromMaster;
+	private HubListener hubListener;
 
-        li = HubDetailDelegate.getLinkInfoFromDetailToMaster(hub);
-        if (li != null) li = OAObjectInfoDelegate.getReverseLinkInfo(li);
-        if (li == null || !li.getRecursive()) {
-            hubRoot.setSharedHub(hub, false);
-            return;
-        }
-        
-        hubMaster = hub.getMasterHub();  // master hub of root hub - this is the 'source' to listen to.
-        if (hubMaster == null) {
-            Hub h = hub;
-            for (;;) {
-                Hub hx = h.getSharedHub();
-                if (hx == null) break;
-                h = hx;
-            }
-            hubCopy = new HubCopy(h, hubRoot, false);
-            return;
-        }
-        
-        propertyFromMaster = HubDetailDelegate.getPropertyFromMasterToDetail(hub);
-        
-        hubListener = new HubListenerAdapter() {
-            @Override
-            public void afterChangeActiveObject(HubEvent e) {
-                if (lastAO != null) { // 20180305 ao could be the same as before
-                    if (lastAO == e.getObject()) return;
-                }
-                HubRoot.this.update();
-            }
-        };
-        hubMaster.addHubListener(hubListener);
-        
-        update();
-    }
-    private Object lastAO;
-    
-    private void update() {
-        if (hubCopy != null) {
-            hubCopy.close();
-            hubCopy = null;
-        }
-        this.hubRoot.clear();
+	/**
+	 * This is used for recursive hubs, so that a Hub will stay at the root. By default, a shared hub that is recursive could change to be
+	 * shared with a child hub. This class is used to make sure that the hub does not change to share a child hub.
+	 *
+	 * @param hubRoot Hub to use as the root, it will auto populated to always be the root hub.
+	 */
+	public HubRoot(Hub hub, Hub hubRoot) {
+		this.hubRoot = hubRoot;
+		if (hub == null) {
+			return;
+		}
+		if (hubRoot == null) {
+			return;
+		}
 
-        OAObject obj = (OAObject) hubMaster.getAO();
-        lastAO = obj;
-        if (obj == null) return;
-        
-        Hub h = (Hub) obj.getProperty(propertyFromMaster);
-        hubCopy = new HubCopy(h, hubRoot, false);
-    }
-    
-    public void close() {
-        if (hubListener != null && hubMaster != null) {
-            hubMaster.removeHubListener(hubListener);
-        }
-    }
-    
+		Class clazz = hub.getObjectClass();
+		OAObjectInfo oi = OAObjectInfoDelegate.getOAObjectInfo(clazz);
+		OALinkInfo li = oi.getRecursiveLinkInfo(OALinkInfo.MANY);
+		if (li == null) {
+			hubRoot.setSharedHub(hub, true);
+			return;
+		}
+
+		li = HubDetailDelegate.getLinkInfoFromDetailToMaster(hub);
+		if (li != null) {
+			li = OAObjectInfoDelegate.getReverseLinkInfo(li);
+		}
+		if (li == null || !li.getRecursive()) {
+			hubRoot.setSharedHub(hub, false);
+			return;
+		}
+
+		hubMaster = hub.getMasterHub(); // master hub of root hub - this is the 'source' to listen to.
+		if (hubMaster == null) {
+			Hub h = hub;
+			for (;;) {
+				Hub hx = h.getSharedHub();
+				if (hx == null) {
+					break;
+				}
+				h = hx;
+			}
+			hubCopy = new HubCopy(h, hubRoot, false);
+			return;
+		}
+
+		propertyFromMaster = HubDetailDelegate.getPropertyFromMasterToDetail(hub);
+
+		hubListener = new HubListenerAdapter() {
+			@Override
+			public void afterChangeActiveObject(HubEvent e) {
+				if (lastAO != null) { // 20180305 ao could be the same as before
+					if (lastAO == e.getObject()) {
+						return;
+					}
+				}
+				HubRoot.this.update();
+			}
+		};
+		hubMaster.addHubListener(hubListener);
+
+		update();
+	}
+
+	private Object lastAO;
+
+	private final AtomicBoolean abUpdate = new AtomicBoolean();
+
+	private void update() {
+		try {
+			for (int i = 0; i < 3; i++) {
+				boolean bx = abUpdate.getAndSet(true);
+				if (!bx) {
+					break;
+				}
+				String s = "concurrent issue, where update is currently running in another thread, will wait 3ms (" + (i + 1)
+						+ " of 3 times), this thread=" + Thread.currentThread();
+				LOG.log(Level.WARNING, s, new Exception(s));
+				Thread.sleep(3);
+			}
+			_update();
+		} catch (Exception e) {
+
+		} finally {
+			abUpdate.set(false);
+		}
+	}
+
+	private void _update() {
+		if (hubCopy != null) {
+			hubCopy.close();
+			hubCopy = null;
+		}
+
+		this.hubRoot.clear();
+
+		OAObject obj = (OAObject) hubMaster.getAO();
+		lastAO = obj;
+		if (obj == null) {
+			return;
+		}
+
+		Hub h = (Hub) obj.getProperty(propertyFromMaster);
+		hubCopy = new HubCopy(h, hubRoot, false);
+	}
+
+	public void close() {
+		if (hubListener != null && hubMaster != null) {
+			hubMaster.removeHubListener(hubListener);
+		}
+	}
+
 }
