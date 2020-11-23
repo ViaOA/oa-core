9ad3fe6be2745aaba9e055bc0aea48eac51baea7
Vince Via <vince@viaoa.com>
2020-11-23 10:48:50 -0500

    version

--- src/main/java/com/viaoa/object/OAObject.java
+++ src/main/java/com/viaoa/object/OAObject.java
@@ -120,7 +120,7 @@ public class OAObject implements java.io.Serializable, Comparable {
 
 	static {
 		// oaversion
-		String ver = "3.7.0.20201110";
+		String ver = "3.7.0.20201123";
 		/*
 		try {
 		    InputStream resourceAsStream = OAObject.class.getResourceAsStream("/META-INF/maven/com.viaoa/oa/pom.properties");
