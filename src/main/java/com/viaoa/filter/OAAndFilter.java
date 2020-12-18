e0de6cc0651ea3329fab1ae97c537a3997af0c82
Vince Via <vvia@oreillyauto.com>
2020-12-18 18:05:02 -0500

    added updateSelect to and filter

--- src/main/java/com/viaoa/filter/OAAndFilter.java
+++ src/main/java/com/viaoa/filter/OAAndFilter.java
@@ -10,25 +10,41 @@
 */
 package com.viaoa.filter;
 
+import com.viaoa.datasource.OASelect;
 import com.viaoa.util.OAFilter;
 
 /**
  * Joins two filters together to create an AND filter between them.
+ *
  * @author vvia
  */
 public class OAAndFilter implements OAFilter {
 
-    private OAFilter filter1, filter2;
-    
-    public OAAndFilter(OAFilter filter1, OAFilter filter2) {
-        this.filter1 = filter1;
-        this.filter2 = filter2;
-    }
-    @Override
-    public boolean isUsed(Object obj) {
-        if (filter1 == null || filter2 == null) return false;
-        if (!filter1.isUsed(obj)) return false;
-        return filter2.isUsed(obj);
-    }
-}
+	private OAFilter filter1, filter2;
 
+	public OAAndFilter(OAFilter filter1, OAFilter filter2) {
+		this.filter1 = filter1;
+		this.filter2 = filter2;
+	}
+
+	@Override
+	public boolean isUsed(Object obj) {
+		if (filter1 != null && !filter1.isUsed(obj)) {
+			return false;
+		}
+		boolean b = (filter2 == null || filter2.isUsed(obj));
+		return b;
+	}
+
+	@Override
+	public boolean updateSelect(OASelect select) {
+		boolean b = false;
+		if (filter1 != null) {
+			b = filter1.updateSelect(select);
+		}
+		if (filter2 != null) {
+			b |= filter2.updateSelect(select);
+		}
+		return b;
+	}
+}
