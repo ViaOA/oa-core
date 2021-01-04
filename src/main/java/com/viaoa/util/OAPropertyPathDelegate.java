521c735161d12c0c533f5a9188d90cf390669027
Vince Via <vvia@oreillyauto.com>
2021-01-04 10:45:26 -0500

    PP delegate

--- src/main/java/com/viaoa/util/OAPropertyPathDelegate.java
+++ src/main/java/com/viaoa/util/OAPropertyPathDelegate.java
@@ -10,18 +10,16 @@
 */
 package com.viaoa.util;
 
-
 public class OAPropertyPathDelegate {
 
-    /**
-     * parse a propertyPath that has a leading "[ClassName]."
-     * @param packageClass class that the from class is in the same package as. 
-     */
-    public static OAPropertyPath createRootPropertyPath(String sPropPath, Class packageClass) throws Exception {
-        OAPropertyPath pp = new OAPropertyPath(packageClass, sPropPath);
-        return pp;
-    }
+	/**
+	 * parse a propertyPath that has a leading "[ClassName]."
+	 * 
+	 * @param packageClass class that the from class is in the same package as.
+	 */
+	public static OAPropertyPath createRootPropertyPath(String sPropPath, Class packageClass) throws Exception {
+		OAPropertyPath pp = new OAPropertyPath(packageClass, sPropPath);
+		return pp;
+	}
 
-    
-    
 }
