3ca85e8fc453899d85cf0db880a4d154158a90bf
Vince Via <vvia@oreillyauto.com>
2022-08-04 06:18:20 -0400

    allow automatch on enum prop

--- src/main/java/com/viaoa/hub/HubAutoMatch.java
+++ src/main/java/com/viaoa/hub/HubAutoMatch.java
@@ -10,6 +10,7 @@
 */
 package com.viaoa.hub;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -17,7 +18,6 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import com.viaoa.object.OALinkInfo;
 import com.viaoa.object.OAObject;
 import com.viaoa.object.OAObjectCallback;
-import com.viaoa.object.OAObjectCallbackDelegate;
 import com.viaoa.object.OAObjectInfo;
 import com.viaoa.object.OAObjectInfoDelegate;
 import com.viaoa.object.OAThreadLocalDelegate;
@@ -73,12 +73,14 @@ public class HubAutoMatch<TYPE, PROPTYPE> extends HubListenerAdapter implements 
 			throw new IllegalArgumentException("hub can not be null");
 		}
 		if (hubMaster == null) {
-			throw new IllegalArgumentException("hubMaster can not be null");
+			//qqqqqqqqqqqqqqqqqq
+			// 20220802 now allows auto match on Enum property
+			// throw new IllegalArgumentException("hubMaster can not be null");
 		}
 
 		this.hub = hub;
 		this.hubMaster = hubMaster;
-		if (!bManuallyCalled) {
+		if (!bManuallyCalled && hubMaster != null) {
 			hubMaster.addHubListener(this);
 		}
 		setProperty(property);
@@ -96,7 +98,9 @@ public class HubAutoMatch<TYPE, PROPTYPE> extends HubListenerAdapter implements 
 	 * Closes HubAutoMatch.
 	 */
 	public void close() {
-		hubMaster.removeHubListener(this);
+		if (hubMaster != null) {
+			hubMaster.removeHubListener(this);
+		}
 	}
 
 	protected void finalize() throws Throwable {
@@ -107,7 +111,7 @@ public class HubAutoMatch<TYPE, PROPTYPE> extends HubListenerAdapter implements 
 	protected void setProperty(String property) {
 		this.property = property;
 		Class c = null;
-		if (property == null || property.length() == 0) {
+		if (hubMaster != null && (property == null || property.length() == 0)) {
 			c = hub.getObjectClass();
 			if (!hubMaster.getObjectClass().equals(c)) {
 				// find property to use
@@ -135,7 +139,7 @@ public class HubAutoMatch<TYPE, PROPTYPE> extends HubListenerAdapter implements 
 			}
 			c = getMethod.getReturnType();
 		}
-		if (!hubMaster.getObjectClass().equals(c)) {
+		if (hubMaster != null && !hubMaster.getObjectClass().equals(c)) {
 			throw new RuntimeException("hubMaster class=" + hubMaster.getObjectClass() + " does not match class for update Hub: " + c);
 		}
 		if (!bManuallyCalled) {
@@ -153,13 +157,18 @@ public class HubAutoMatch<TYPE, PROPTYPE> extends HubListenerAdapter implements 
 			if (HubDelegate.getCurrentState(hub, null, null) != HubDelegate.HubCurrentStateEnum.InSync) {
 				return;
 			}
-			if (HubDelegate.getCurrentState(hubMaster, null, null) != HubDelegate.HubCurrentStateEnum.InSync) {
+			if (hubMaster != null && HubDelegate.getCurrentState(hubMaster, null, null) != HubDelegate.HubCurrentStateEnum.InSync) {
 				return;
 			}
 			if (bServerSideOnly) {
 				OARemoteThreadDelegate.sendMessages(true);
 			}
-			_update1();
+
+			if (hubMaster != null) {
+				_update1();
+			} else {
+				_update2();
+			}
 		} finally {
 			abUpdating.set(false);
 			if (bServerSideOnly) {
@@ -191,24 +200,24 @@ public class HubAutoMatch<TYPE, PROPTYPE> extends HubListenerAdapter implements 
 			}
 			// see if object is in hubx
 			if (getMethod == null) {
-			    if (hubx.getObject(obj) == null) {
-			        if (hubx.getAllowAdd(OAObjectCallback.CHECK_AllButProcessed, obj)) {
-                        hubx.add(obj);
-				    }
-				    /* 20210514 was:
-                    if (hubx.getEnabled()) {
-                        hubx.add(obj);
-                    }
-                    */
+				if (hubx.getObject(obj) == null) {
+					if (hubx.getAllowAdd(OAObjectCallback.CHECK_AllButProcessed, obj)) {
+						hubx.add(obj);
+					}
+					/* 20210514 was:
+					if (hubx.getEnabled()) {
+					    hubx.add(obj);
+					}
+					*/
 				}
 			} else {
 				for (int j = 0;; j++) {
 					Object o = hubx.elementAt(j);
 					if (o == null) {
-	                    if (hubx.getAllowAdd(OAObjectCallback.CHECK_AllButProcessed, obj)) {
-                            createNewObject((PROPTYPE) obj);
-	                    }
-	                    /* 20210514 was:
+						if (hubx.getAllowAdd(OAObjectCallback.CHECK_AllButProcessed, obj)) {
+							createNewObject((PROPTYPE) obj);
+						}
+						/* 20210514 was:
 						if (hubx.getEnabled()) {
 						    createNewObject((PROPTYPE) obj);
 						}
@@ -257,6 +266,46 @@ public class HubAutoMatch<TYPE, PROPTYPE> extends HubListenerAdapter implements 
 		}
 	}
 
+	private int maxEnumValue;
+	private boolean bMaxEnumValueChecked;
+
+	private void _update2() {
+		if (!bMaxEnumValueChecked) {
+			maxEnumValue = findMaxEnumValue();
+			bMaxEnumValueChecked = false;
+		}
+
+		for (int i = hub.getSize(); i <= maxEnumValue; i++) {
+			createNewObject(i);
+		}
+	}
+
+	private int findMaxEnumValue() {
+		Class cz = hub.getObjectClass();
+
+		Field field = null;
+
+		String name = "hub" + property;
+
+		for (Field f : cz.getFields()) {
+			if (name.equalsIgnoreCase(f.getName())) {
+				field = f;
+				break;
+			}
+		}
+
+		int max = 0;
+		try {
+			Object objx = field.get(null);
+			if (objx instanceof Hub) {
+				max = ((Hub) objx).getSize() - 1;
+			}
+		} catch (Exception ex) {
+			//qqq
+		}
+		return max;
+	}
+
 	/**
 	 * Called before removing an object that does not have a matching value.
 	 */
@@ -264,7 +313,7 @@ public class HubAutoMatch<TYPE, PROPTYPE> extends HubListenerAdapter implements 
 		return true;
 	}
 
-	protected TYPE createNewObject(PROPTYPE obj) {
+	protected TYPE createNewObject(Object obj) {
 		TYPE object;
 		try {
 			object = (TYPE) hub.getObjectClass().newInstance();
