2a97bef0aec5ffa4e8b39d7f0997f0b375892893
Vince Via <vince@viaoa.com>
2023-06-30 21:03:28 -0400

    oaregex started

---
+++ src/main/java/com/viaoa/util/OARegex.java
@@ -0,0 +1,15 @@
+package com.viaoa.util;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class OARegex {
+
+/*
+
+> comma separated with 0+ whitespace chars
+   List<String> al = Arrays.asList(s.split(",\\s*"));     
+    
+*/    
+    
+}
