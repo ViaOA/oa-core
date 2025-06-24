0999f44b7d7db6c0ad4ae03bd5626ffea9fdc037
Vince Via <vince@viaoa.com>
2025-06-24 13:51:28 -0400

    version 3.7.12

--- src/main/java/com/viaoa/object/OAObject.java
+++ src/main/java/com/viaoa/object/OAObject.java
@@ -125,8 +125,8 @@ public class OAObject implements java.io.Serializable, Comparable {
 	static {
 		// oaversion
 	    
-	    // pom version: 3.7.11
-		String ver = "3.7.11.202504050";
+	    // pom version: 3.7.12
+		String ver = "3.7.12.202506230";
 		
 		/*
 		 *  previous:
@@ -141,7 +141,7 @@ public class OAObject implements java.io.Serializable, Comparable {
          *  String ver = "3.7.8.202405070";
          *  String ver = "3.7.9.202407150";
          *  String ver = "3.7.10.202409160";
-
+         *  String ver = "3.7.11.202504050";
 		 */
 		/*
 		try {
